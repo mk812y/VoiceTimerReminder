5b0b08a2884f2be40dc68d74eb2f218b591b8cc2
mk812y <mk812y@gmail.com>
2022-09-13 17:14:40 +0300

    TimerAddNew настроен title и picker

--- VoiceTimerReminder/View/TimerAddNew.swift
+++ VoiceTimerReminder/View/TimerAddNew.swift
@@ -8,52 +8,55 @@
 import SwiftUI
 
 struct TimerAddNew: View {
-    @State private var name = "Timer 1"
+    @State private var name = "Timer"
     @State private var hours = [Int](0...23)
     @State private var selectedHours = 1
     @State private var minutes = [Int](0...59)
     @State private var selectedMinutes = 30
     
     var body: some View {
-        VStack {
-            Section(header: Text("dsf")) {
-                HStack {
+            Form {
+                Section(header: Text("Timer name")) {
+                    TextField("sdf", text: $name)
+                }
+                Section(header: Text("Timer time")) {
                     HStack {
-                        Picker("sd", selection: $selectedHours) {
-                            ForEach(hours, id: \.self) { hour in
-                                Text("\(hour)")
-                                    .font(.title)
+                        HStack {
+                            Picker("sd", selection: $selectedHours) {
+                                ForEach(hours, id: \.self) { hour in
+                                    Text("\(hour)")
+                                        .font(.title)
+                                }
                             }
+                            .pickerStyle(.wheel)
+                            .frame(width: 40)
+                            .clipped()
+                            Text("hours")
+                                .font(.title2)
                         }
-                        .pickerStyle(.wheel)
-                        .frame(width: 40, height: 90)
-                        .clipped()
-                        Text("hours")
-                            .font(.title2)
-                    }
-                    .padding(EdgeInsets(top: 15, leading: 15, bottom: 15, trailing: 50))
-                    HStack {
-                        Picker("", selection: $selectedMinutes) {
-                            ForEach(minutes, id: \.self) { minute in
-                                Text("\(minute)")
-                                    .font(.title)
+                        .padding(EdgeInsets(top: 0, leading: 15, bottom: 0, trailing: 50))
+                        HStack {
+                            Picker("", selection: $selectedMinutes) {
+                                ForEach(minutes, id: \.self) { minute in
+                                    Text("\(minute)")
+                                        .font(.title)
+                                }
                             }
+                            .pickerStyle(.wheel)
+                            .frame(width: 40)
+                            .clipped()
+                            Text("min")
+                                .font(.title2)
                         }
-                        .pickerStyle(.wheel)
-                        .frame(width: 40, height: 90)
-                        .clipped()
-                        Text("min")
-                            .font(.title2)
+                        .padding(.horizontal)
                     }
-                    .padding()
+                }
+                Section(header: Text("Theme color")) {
+                    TextField("sdf", text: $name)
                 }
             }
-            Section {
-                TextField("sdf", text: $name)
-            }
-            .navigationTitle("Settings")
+            .navigationTitle("Add Timer")
             .navigationBarTitleDisplayMode(.inline)
-        }
     }
 }
 

22034024296acae160802ba3819487cfbcef3727
mk812y <mk812y@gmail.com>
2022-09-13 14:04:53 +0300

    TimerAddNew

---
+++ VoiceTimerReminder/View/TimerAddNew.swift
@@ -0,0 +1,20 @@
+//
+//  TimerAddNew.swift
+//  VoiceTimerReminder
+//
+//  Created by Михаил Куприянов on 13.09.2022.
+//
+
+import SwiftUI
+
+struct TimerAddNew: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TimerAddNew_Previews: PreviewProvider {
+    static var previews: some View {
+        TimerAddNew()
+    }
+}

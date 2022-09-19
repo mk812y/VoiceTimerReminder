f8eb0b6f91691dfc6badfe78431ca5949c2466f6
mk812y <mk812y@gmail.com>
2022-09-19 22:04:05 +0300

    CircularButtonView done

--- VoiceTimerReminder/Helpers/CircularButtonView.swift
+++ VoiceTimerReminder/Helpers/CircularButtonView.swift
@@ -8,22 +8,57 @@
 import SwiftUI
 
 struct CircularButtonView: View {
+    
+    enum ButtonStyle {
+        case start
+        case stop
+//        case pause
+        
+        var title: String {
+            switch self {
+            case .start:
+                return "Start"
+            case .stop:
+                return "Stop"
+            }
+        }
+        
+        var fillColor: Color {
+            switch self {
+            case .start:
+                return Color(.systemGreen)
+            case .stop:
+                return Color(.orange)
+            }
+        }
+        
+        var foregroundColor: Color {
+            switch self {
+            case .start:
+                return Color(.white)
+            case .stop:
+                return Color(.white)
+            }
+        }
+
+    }
+    var style: ButtonStyle
     var width: CGFloat
     
     var body: some View {
         Circle()
-            .fill(.green)
+            .fill(style.fillColor)
             .frame(width: width)
             .overlay(
-                Text("Start")
+                Text(style.title)
                     .font(.headline)
-                    .foregroundColor(.white)
+                    .foregroundColor(style.foregroundColor)
             )
     }
 }
 
 struct CircularButtonView_Previews: PreviewProvider {
     static var previews: some View {
-        CircularButtonView(width: 80)
+        CircularButtonView(style: .start, width: 80)
     }
 }
